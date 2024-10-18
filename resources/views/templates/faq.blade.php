857866465be23f8a7c03fc04eeeb7d3cf699afec
VereAbsolutum <sergiopile3@gmail.com>
2024-10-17 23:43:47 -0300

    func: add section-header to faq section

--- resources/views/templates/faq.blade.php
+++ resources/views/templates/faq.blade.php
@@ -1,5 +1,10 @@
 <section id="faq" class="faq">
     <div class="container mx-auto px-4">
+        <x-molecules.section-header
+            title="FAQ"
+            subtitle="O que mais perguntam."
+            subheader="Ainda tem dúvidas? Entre em contato agora mesmo. Ficamos feliz em respondê-lo."
+        />
         <x-tw.layouts.container>
             <x-tw.display.accordion :items="$questions" />
         </x-tw.layouts.container>
